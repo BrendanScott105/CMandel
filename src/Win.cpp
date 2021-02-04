074025d56a2208d9835533beb4c7669373884737
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2021-02-04 17:18:14 -0500

    About menu polish

--- src/Win.cpp
+++ src/Win.cpp
@@ -53,7 +53,9 @@ LRESULT CALLBACK Proc(HWND hWnd, UINT defmsg, WPARAM wp, LPARAM lp) // window pr
 			LocationMenu(hWnd); // Call location menu
 			break;
 		case 4: // Help box
-			CreateWindowW(L"static", L" About CMandel...\n\n © 2021, Brendan Scott\n\n This is an open source program :\n Github.com/BrendanScott105/CMandel\n\n Controls :\n W / A / S / D : Up / Left / Down / Right\n Mouse left : Zoom in\n Mouse right : Zoom out\n - / + : Increase / Decrease iterations", WS_VISIBLE | WS_BORDER | WS_CHILD, 100, 150, 300, 200, hWnd, NULL, NULL, NULL);
+			CreateWindowW(L"static", L" About CMandel...   © 2021, Brendan Scott\n\n This is open source software :\n Github.com/BrendanScott105/CMandel\n\n Controls :\n W / A / S / D : Up / Left / Down / Right\n Mouse left : Zoom in\n Mouse right : Zoom out\n - / + : Increase / Decrease iterations\n\n Info bar :\n i - Iters | X - Real | Y - Imaginary | Z - Zoom\n\n Limitations :\n - Iterations does not exceed 999999\n - Zoom limited to 2^64\n - Precision limited to 64 Bits\n - Resolution locked at 500x500", WS_VISIBLE | WS_BORDER | WS_CHILD, 100, 95, 300, 310, hWnd, NULL, NULL, NULL);
+			CreateWindowW(L"static", L" ╳", WS_VISIBLE | WS_BORDER | WS_CHILD, 379, 95, 21, 20, hWnd, NULL, NULL, NULL);
+			CreateWindowW(L"static", L"", WS_VISIBLE | WS_BORDER | WS_CHILD, 100, 114, 300, 1, hWnd, NULL, NULL, NULL);
 			break;
 		case 5: // exit button
 			exit(0); // exit with code 0
@@ -105,22 +107,23 @@ void WinMenus(HWND hWnd) // window menu code
 
 void InfoBar(HWND hWnd) // add current view information bar
 {
-	CreateWindowW(L"static", L"Iterations :", WS_VISIBLE | WS_BORDER | WS_CHILD, 0, 500, 150, 19, hWnd, NULL, NULL, NULL);
-	CreateWindowW(L"static", L"Real :", WS_VISIBLE | WS_BORDER | WS_CHILD, 150, 500, 175, 19, hWnd, NULL, NULL, NULL);
-	CreateWindowW(L"static", L"Imag :", WS_VISIBLE | WS_BORDER | WS_CHILD, 325, 500, 175, 19, hWnd, NULL, NULL, NULL);
+	CreateWindowW(L"static", L"i:200", WS_VISIBLE | WS_BORDER | WS_CHILD, -1, 500, 59, 19, hWnd, NULL, NULL, NULL);
+	CreateWindowW(L"static", L"X:0.0000000000000", WS_VISIBLE | WS_BORDER | WS_CHILD, 57, 500, 132, 19, hWnd, NULL, NULL, NULL);
+	CreateWindowW(L"static", L"Y:0.0000000000000", WS_VISIBLE | WS_BORDER | WS_CHILD, 188, 500, 133, 19, hWnd, NULL, NULL, NULL);
+	CreateWindowW(L"static", L"Z:4", WS_VISIBLE | WS_BORDER | WS_CHILD, 320, 500, 181, 19, hWnd, NULL, NULL, NULL);
 }
 
 void FormulaMenu(HWND hWnd) // Create formula menu
 {
-	CreateWindowW(L"static", L" Select Formula...", WS_VISIBLE | WS_BORDER | WS_CHILD, 150, 150, 200, 200, hWnd, NULL, NULL, NULL);
+	HWND Formula = CreateWindowW(L"static", L" Select Formula...", WS_VISIBLE | WS_BORDER | WS_CHILD , 150, 150, 200, 200, hWnd, NULL, NULL, NULL);
 }
 
 void ColorMenu(HWND hWnd) // Create color menu
 {
-	CreateWindowW(L"static", L" Choose color preset...", WS_VISIBLE | WS_BORDER | WS_CHILD, 150, 150, 200, 200, hWnd, NULL, NULL, NULL);
+	HWND Color = CreateWindowW(L"static", L" Choose color preset...", WS_VISIBLE | WS_BORDER | WS_CHILD , 150, 150, 200, 200, hWnd, NULL, NULL, NULL);
 }
 
 void LocationMenu(HWND hWnd) // Create location menu
 {
-	CreateWindowW(L"static", L" Set location and zoom...", WS_VISIBLE | WS_BORDER | WS_CHILD, 150, 175, 200, 150, hWnd, NULL, NULL, NULL);
+	HWND Location = CreateWindowW(L"static", L" Set location and zoom...", WS_VISIBLE | WS_BORDER | WS_CHILD , 150, 175, 200, 150, hWnd, NULL, NULL, NULL);
 }
